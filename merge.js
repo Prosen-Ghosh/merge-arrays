af31954ee5a8576e36903af26153dad96f03d341
Prosen-Ghosh <prosenghosh25@gmail.com>
2017-10-04 17:04:31 +0600

    spread Operator used

--- merge.js
+++ merge.js
@@ -2,7 +2,7 @@ const merge = (function(){
     'use strict';
     const fn = function(){
         if(arguments.length < 2)throw new Error('expect at least 2 argument');
-        return Array.from(arguments).reduce((p,c) => {
+        return [...arguments].reduce((p,c) => {
             if(Object.prototype.toString.call(c).toLowerCase() === '[object array]')return [...p, ...c];
             else throw new TypeError('expets array arguments got ' + typeof c);
         },[]);
